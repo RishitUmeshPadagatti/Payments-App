cc9b10fe1258feabbfb0f1afd7f9563b8ffa0555
Rishit Umesh Padagatti <rishitpadagatti@gmail.com>
2025-04-11 13:14:31 +0530

    init

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,44 @@
 const express = require('express')
-const mainRouter = require("./routers/index")
-const { User } = require("./db")
 const cors = require("cors")
+// const { PrismaClient } = require('@prisma/client')
+const {PrismaClient} = require("./generated/prisma")
 
 const app = express()
+const prisma = new PrismaClient()
 
 app.use(express.json())
 app.use(cors())
 
-app.use("/api/v1", mainRouter)
+app.get("/", async (req, res) => {
+    try {
+        const shops = await prisma.shop.findMany()
+        res.json(shops)
+      } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: "Something went wrong" })
+      }
+})
+
+app.get("/types", async (req, res) => {
+    try {
+        res.json(["CAFE", "BOOKSTORE", "PLANT", "STATIONARY", "BAKERY"])
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: "Something went wrong" })
+    }
+})
+
+app.get("/business", async(req, res) => {
+    try {
+        const id = req.query.id;
+
+        const data = await prisma.shop.findUnique({where: {id: id}})
+        res.json(data)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: "Something went wrong" })
+    }
+})
 
 app.use(function (err, req, res, next){
     console.log(err + "\nERROR\n\n\n");
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,15 @@
-import { Suspense, lazy } from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-const Signup = lazy(() => import ("./pages/Signup"))
-const Signin = lazy(() => import ("./pages/Signin"))
-const Dashboard = lazy(() => import ("./pages/Dashboard"))
-const SendMoney = lazy(() => import ("./pages/SendMoney"))
+import {Dashboard} from "./pages/Dashboard"
+
+
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/signup" element={<Suspense fallback="Loading..."><Signup /></Suspense>} />
-        <Route path="/signin" element={<Suspense fallback="Loading..."><Signin /></Suspense>} />
-        <Route path="/dashboard" element={<Suspense fallback="Loading..."><Dashboard /></Suspense>} />
-        <Route path="/send" element={<Suspense fallback="Loading..."><SendMoney /></Suspense>} />
-        <Route path="/" element={<Suspense fallback="Loading..."><Dashboard /></Suspense>} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/business/:id" element={<Business />} />
       </Routes>
     </BrowserRouter>
   )
